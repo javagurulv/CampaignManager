d81b62858e8104f8476bfc6cc0b7497f9651020e
Viktor Savonin <viktor.savonin@meawallet.com>
2016-11-08 16:48:29 +0200

    add gradlew

---
+++ api/src/main/java/lv/javaguru/campaignmanager/api/dto/builders/CampaignGroupDTOBuilder.java
@@ -0,0 +1,43 @@
+package lv.javaguru.campaignmanager.api.dto.builders;
+
+
+import lv.javaguru.campaignmanager.api.dto.CampaignGroupDTO;
+
+import java.util.Date;
+
+public class CampaignGroupDTOBuilder {
+
+    private Long id;
+    private String title;
+    private Date createdDate;
+
+    private CampaignGroupDTOBuilder() {}
+
+    public static CampaignGroupDTOBuilder createCampaignGroupDTO() {
+        return new CampaignGroupDTOBuilder();
+    }
+
+    public CampaignGroupDTO build() {
+        CampaignGroupDTO dto = new CampaignGroupDTO();
+        dto.setId(id);
+        dto.setTitle(title);
+        dto.setCreatedDate(createdDate);
+        return dto;
+    }
+
+    public CampaignGroupDTOBuilder withId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public CampaignGroupDTOBuilder withTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public CampaignGroupDTOBuilder withCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+        return this;
+    }
+
+}
